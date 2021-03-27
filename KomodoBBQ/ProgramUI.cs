e39170e0135114f351ab0650f8e0f6bf86c13977
ZarichDataSciences <nathan.zarich@gmail.com>
2021-03-26 21:37:02 -0400

    added KomodoBBQ Program, ProgramUI and REPO Classes

---
+++ KomodoBBQ/ProgramUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoBBQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ KomodoBBQ/repo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoBBQ
+{
+    public class repo
+    {
+        public int NumberHamburgersServed { get; set; }
+        public int NumberHotDogsServed { get; set; }
+        public int NumberVeggieBurgersServed { get; set; }
+        public int NumberIceCreamServed { get; set; }
+        public int NumberPopcornBagsServed { get; set; }
+
+
+    }
+}
