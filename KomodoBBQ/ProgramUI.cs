22858081efc0ee18fcf3e895bf3394563399ba90
ZarichDataSciences <nathan.zarich@gmail.com>
2021-03-30 18:40:10 -0400

    added Komodo BBQ User UI menu items and expanded REPO

--- KomodoBBQ/ProgramUI.cs
+++ KomodoBBQ/ProgramUI.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace KomodoBBQ
 {
-    class Program
+    class ProgramUI
     {
         static void Main(string[] args)
         {
--- KomodoBBQ/repo.cs
+++ KomodoBBQ/repo.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace KomodoBBQ
 {
-    public class repo
+    public class FoodSalesRepo
     {
         public int NumberHamburgersServed { get; set; }
         public int NumberHotDogsServed { get; set; }
         public int NumberVeggieBurgersServed { get; set; }
         public int NumberIceCreamServed { get; set; }
         public int NumberPopcornBagsServed { get; set; }
-
-
+        public int CostOfPopcorn { get; set; }
+        
+        public decimal CostOfPackaging = .25m; //set at 25 cents for all packaging and condiments for all products
     }
 }
